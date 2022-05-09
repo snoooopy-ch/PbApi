2be6d152054ba432ad5731146feaee2ba7a32918
snoooopy-ch <snoopy.850807@gmail.com>
2022-05-09 03:30:49 -0500

    init

--- source/routes/posts.ts
+++ source/routes/posts.ts
@@ -7,5 +7,8 @@ const cache = apicache.middleware;
 
 router.get('/api/posts', cache('5 minutes'), controller.getPosts);
 router.get('/api/ping', controller.getPing);
+router.get('/add', function (req, res) {
+    res.json({"error": false, "message":"Hello!"})
+})
 
 export = router;
